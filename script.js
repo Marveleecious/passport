3d684eb1c0423ca316721345a05e76061d0b94dc
Marveleecious <richardtimilehin05@gmail.com>
2022-10-10 16:32:48 -0700

    second statement

--- script.js
+++ script.js
@@ -13,19 +13,25 @@ function emails(){
      document.getElementById('emails').innerHTML = email;
      output.value = email.toLowerCase()
 }
-function phoneno(){
-    let phoneNumber;
-    let num=document.getElementById('phone')
-     number=num.value
 
-     if (number.startsWith('081') || number.startsWith('080') || number.startsWith('091') || number.startsWith('090')  || number.startsWith('070') || number.startsWith('071') ) {
+
+function phoneno(){
+    let input = document.getElementById('phone');
+    let output = document.getElementById('phoneno');
+    output.innerHTML = input.value
+    let number = document.getElementById('phone').value;
+
+    if (number.startsWith('081') || number.startsWith('080') || number.startsWith('091') || number.startsWith('090')  || number.startsWith('070') || number.startsWith('071') ) {
         if (number.length== 11 ){
-        phoneNumber = number;
+            valid = input.value
         }
-    }
-        else if (phoneNumber.length > 11){
-            phoneNumber = 'Invalid number';
-        
+     else{
+        valid = "invalid"
      }
-     document.getElementById('phoneno').innerHTML = phoneNumber;
+    }
+    else{
+        valid = "invalid"
+    }
+    
+    document.getElementById('phoneno').innerHTML = valid;
 }
